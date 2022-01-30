c58363ad09bb4e4e21fadaeb9e119552f82cef6a
endoresur <mlhv2lv@gmail.com>
2022-01-30 18:35:07 +0500

    Add lose of game

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 
 let character = document.getElementById("character");
-let block = document.getElementsById("block");
+let block = document.getElementById("block");
 
 function jump() {
-    if (character.classList != "animate") { 
-        character.classList.add("animate");
-    }
+    if (character.classList == "animate") {return;}
+    character.classList.add("animate");
+
     setTimeout(function() {
         character.classList.remove("animate");
-    }, 500);
+    }, 500);    
 }
+
+let checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    //console.log(characterTop);
+
+    if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
+            block.style.animation = "none";
+            block.style.display = "none";
+            alert("u lose");
+
+    }
+}, 10);
